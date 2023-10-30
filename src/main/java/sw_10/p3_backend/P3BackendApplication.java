ffbe6ba35299fbc0770706762f6dea332b595487
EmilChri <113058804+EmilChri@users.noreply.github.com>
2023-10-30 14:11:31 +0100

    VI ER I GANG MED AT KODE!!!!!

---
+++ src/main/java/sw_10/p3_backend/P3BackendApplication.java
@@ -0,0 +1,11 @@
+package sw_10.p3_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3BackendApplication {
+
+    public static void main(String[] args) {SpringApplication.run(P3BackendApplication.class, args);}
+
+}
---
+++ src/test/java/sw_10/p3_backend/P3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sw_10.p3_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
