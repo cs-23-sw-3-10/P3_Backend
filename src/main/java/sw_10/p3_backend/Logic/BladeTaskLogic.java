8e646cd7e4f3ce3480ceb2023204059f3bbac0e5
Lazcse <lassefonager@hotmail.com>
2023-10-31 09:31:20 +0100

    Created our classes

--- src/main/java/sw_10/p3_backend/Logic/BladeTaskLogic.java
+++ src/main/java/sw_10/p3_backend/Logic/BladeTaskLogic.java
@@ -3,5 +3,5 @@ package sw_10.p3_backend.Logic;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ScheduleLogic {
+public class BladeTaskLogic {
 }
---
+++ src/main/java/sw_10/p3_backend/Repository/BladeProjectRepository.java
@@ -0,0 +1,7 @@
+package sw_10.p3_backend.Repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BladeProjectRepository {
+}
