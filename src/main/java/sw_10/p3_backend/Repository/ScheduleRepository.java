790cd2a5bac1e6437a94a5c34fbb90cf049b47f2
Lazcse <lassefonager@hotmail.com>
2023-11-01 13:11:31 +0100

    Made all repositories extend JpaRepository

--- src/main/java/sw_10/p3_backend/Repository/ScheduleRepository.java
+++ src/main/java/sw_10/p3_backend/Repository/ScheduleRepository.java
@@ -1,7 +1,9 @@
 package sw_10.p3_backend.Repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import sw_10.p3_backend.Model.Schedule;
 
 @Repository
-public interface ScheduleRepository {
+public interface ScheduleRepository extends JpaRepository<Schedule,Long> {
 }
--- src/main/java/sw_10/p3_backend/Repository/TechnicianRepository.java
+++ src/main/java/sw_10/p3_backend/Repository/TechnicianRepository.java
@@ -1,7 +1,9 @@
 package sw_10.p3_backend.Repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import sw_10.p3_backend.Model.Technician;
 
 @Repository
-public interface TechnicianRepository {
+public interface TechnicianRepository extends JpaRepository<Technician,Long>{
 }
