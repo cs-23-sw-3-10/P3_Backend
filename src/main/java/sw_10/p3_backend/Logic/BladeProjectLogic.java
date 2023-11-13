7adee3bee654022a90ed0b055331c66a2be44314
sebub <ssma22@student.aau.dk>
2023-11-13 10:47:25 +0100

    Create/delete BP
    Fixed create BP added delete added testes

---
+++ src/main/java/sw_10/p3_backend/Logic/BladeProjectLogic.java
@@ -0,0 +1,51 @@
+package sw_10.p3_backend.Logic;
+
+
+import org.springframework.stereotype.Service;
+import sw_10.p3_backend.Model.BladeProject;
+import sw_10.p3_backend.Model.Schedule;
+import sw_10.p3_backend.Repository.BladeProjectRepository;
+import sw_10.p3_backend.Repository.ScheduleRepository;
+import sw_10.p3_backend.exception.IdNotFoundException;
+import sw_10.p3_backend.exception.ScheduleCreationException;
+import sw_10.p3_backend.exception.ScheduleNotFoundException;
+
+import java.util.List;
+
+
+@Service
+public class BladeProjectLogic {
+
+
+    private final BladeProjectRepository BladeProjectRepository;
+    private final ScheduleRepository scheduleRepository;
+
+
+    public BladeProjectLogic(BladeProjectRepository bladeProjectRepository, ScheduleRepository scheduleRepository){
+        this.BladeProjectRepository = bladeProjectRepository;
+        this.scheduleRepository = scheduleRepository;
+    }
+
+    public BladeProject createProject(String name, String customer, String projectLeader) {
+            Schedule schedule = scheduleRepository.findScheduleByIsActive(false);//Makes sure all new assigned projects are assigned to the draft schedule
+            BladeProject project = new BladeProject(schedule, name, customer, projectLeader);
+            BladeProjectRepository.save(project);
+            return project;
+    }
+
+    public String deleteProject(Long id) {
+        BladeProject project = BladeProjectRepository.findById(id).orElseThrow(() -> new IdNotFoundException("Project with id " + id + " not found"));
+        if(project.getBladeTasks().isEmpty()) {//makes sure the project has no tasks before deleting
+            BladeProjectRepository.deleteById(id);
+            return "Project deleted";
+        }
+        else {
+            return "Project has tasks";//Create logic to delete tasks before deleting project
+        }
+    }
+
+    public List<BladeProject> findAll(){
+        return BladeProjectRepository.findAll();
+    }
+}
+
---
+++ src/test/java/sw_10/p3_backend/Logic/BladeProjectLogicTest.java
@@ -0,0 +1,81 @@
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import sw_10.p3_backend.Logic.BladeProjectLogic;
+import sw_10.p3_backend.Model.BladeProject;
+import sw_10.p3_backend.Model.BladeTask;
+import sw_10.p3_backend.Model.Schedule;
+import sw_10.p3_backend.Repository.BladeProjectRepository;
+import sw_10.p3_backend.Repository.ScheduleRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public class BladeProjectLogicTest {
+
+    @Mock
+    private ScheduleRepository scheduleRepository;
+
+    @Mock
+    private BladeProjectRepository bladeProjectRepository;
+
+    @InjectMocks
+    private BladeProjectLogic bladeProjectLogic;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testCreateProject() {
+        // Arrange
+        String name = "Test Project";
+        String customer = "Test Customer";
+        String projectLeader = "Test Leader";
+        Schedule mockSchedule = new Schedule(); // Assuming Schedule is a valid class
+        when(scheduleRepository.findScheduleByIsActive(false)).thenReturn(mockSchedule);
+
+        BladeProject expectedProject = new BladeProject(mockSchedule, name, customer, projectLeader);
+
+        // Act
+        BladeProject result = bladeProjectLogic.createProject(name, customer, projectLeader);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(expectedProject.getProjectName(), result.getProjectName());
+        assertEquals(expectedProject.getCustomer(), result.getCustomer());
+        assertEquals(expectedProject.getProjectLeader(), result.getProjectLeader());
+
+        verify(scheduleRepository).findScheduleByIsActive(false);
+        verify(bladeProjectRepository).save(any(BladeProject.class));
+    }
+
+    @Test
+    public void testDeleteProject() {
+        // Arrange
+        Long id = 1L;
+        Long id2 = 2L;
+        BladeProject mockProject = new BladeProject();
+        BladeTask mockTask = new BladeTask();
+        List<BladeTask> mockTaskList = List.of(mockTask);
+        BladeProject mockProjectWithTasks = new BladeProject();// Assuming BladeTask is a valid class
+        mockProjectWithTasks.setBladeTasks(mockTaskList);
+        when(bladeProjectRepository.findById(id)).thenReturn(Optional.of(mockProject));
+        when(bladeProjectRepository.findById(id2)).thenReturn(Optional.of(mockProjectWithTasks));
+
+        // Act
+        String resultNoTasks = bladeProjectLogic.deleteProject(id);
+        String resultTasks = bladeProjectLogic.deleteProject(id2);
+
+        // Assert
+        assertEquals("Project deleted", resultNoTasks);
+        assertEquals("Project has tasks", resultTasks);
+    }
+}
