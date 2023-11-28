e1426e668f3acfab58f8779bed85a8bf68fbcbfc
SstormM <sebbemain@gmail.com>
2023-11-28 17:20:42 +0100

    Update ConflictLogic.java

--- src/main/java/sw_10/p3_backend/Logic/ConflictLogic.java
+++ src/main/java/sw_10/p3_backend/Logic/ConflictLogic.java
@@ -19,17 +19,16 @@ public class ConflictLogic {
 
     private final ConflictRepository conflictRepository;
 
-    public ConflictLogic(ConflictRepository conflictRepository){
+    public ConflictLogic(ConflictRepository conflictRepository) {
         this.conflictRepository = conflictRepository;
     }
 
-    public List<Conflict> allConflicts(){
+    public List<Conflict> allConflicts() {
         return conflictRepository.findAll();
     }
 
 
-
-    public void createConflict(Booking booking, BladeTask bladeTask){
+    public void createConflict(Booking booking, BladeTask bladeTask) {
         //TODO: Add associated bladeTask to conflict
         Conflict conflict = new Conflict(booking, bladeTask);
         conflictRepository.save(conflict);
@@ -37,13 +36,9 @@ public class ConflictLogic {
 
     //TODO: Write log to update conflicts when bookings delete or changed (currently only deletes conflicts when associated booking is deleted)
     public void removeConflicts(List<Booking> bookings) {
-        for (Booking booking: bookings) {
+        for (Booking booking : bookings) {
             List<Conflict> conflicts = conflictRepository.findAllByBooking(booking);
             conflictRepository.deleteAll(conflicts);
         }
     }
-
-    public Flux<String> getGreetingsStream() {
-        return Mono.delay(Duration.ofMillis(5000))
-                .flatMapMany(aLong -> Flux.just("Hi!", "Bonjour!", "Hola!", "Ciao!", "Zdravo!"));
-    }}
+}
