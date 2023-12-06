a91d841a0d37a5cf97e4372eceae6cb28be8c268
SstormM <sebbemain@gmail.com>
2023-12-06 21:36:40 +0100

    Update BladeProjectLogic.java

--- src/main/java/sw_10/p3_backend/Logic/BladeProjectLogic.java
+++ src/main/java/sw_10/p3_backend/Logic/BladeProjectLogic.java
@@ -106,6 +106,9 @@ public class BladeProjectLogic {
             }
         };
 
+        bladeProject.setStartDate(finalStartDate);
+        bladeProject.setEndDate(finalEndDate);
+
         //Create bookings if there are existing resource orders and no bookings
         if(!bladeProject.getResourceOrders().isEmpty() && bladeProject.getBookings().isEmpty()){
             bookingLogic.createBookings(bladeProject.getResourceOrders(), bladeProject);
@@ -114,6 +117,7 @@ public class BladeProjectLogic {
         if(bladeProject.getStartDate() == null || bladeProject.getEndDate() == null || finalStartDate != null && finalStartDate.isBefore(bladeProject.getStartDate()) || finalEndDate != null && finalEndDate.isAfter(bladeProject.getEndDate())) {
             bookingLogic.updateBookings(bladeProject, finalStartDate, finalEndDate);
         }
+
 
 
         bladeProjectRepository.save(bladeProject);
